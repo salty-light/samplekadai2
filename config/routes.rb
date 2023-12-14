755879ec3acdb3b4ce4f25ca8ab3ea9689cd6678
Raichi Ando <135943914+salty-light@users.noreply.github.com>
2023-12-14 06:52:53 +0000

    7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 get 'homes', to: 'homes#top'
+get 'homes/about', to: 'homes#about', as: :about
+root to: 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
