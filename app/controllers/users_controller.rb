92c8e269a972fec9dbbafbf25c84984a1cc1d068
Raichi Ando <135943914+salty-light@users.noreply.github.com>
2023-12-22 07:27:51 +0000

    16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
   def edit
   @user = User.find(params[:id])
   end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'プロフィールが更新されました'
+    else
+      render :edit
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
